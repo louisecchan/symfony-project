468e4d0c48768fd903358ab71f8ef6fb0166db15
louisechan <louise.chan@101medialab.com>
2024-04-26 22:13:41 +0800

    Add custom page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,18 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="app_main")
+     * @Route("/", name="index")
      */
-    public function index(): JsonResponse
+    public function index()
     {
-        return $this->json([
-            'message' => 'Welcome freeCodeCamp!',
-        ]);
+        return new Response('<h1>Welcome freeCodeCamp</h1>');
+    }
+
+    /**
+     * @Route("/custom", name="custom")
+     */
+    public function custom() {
+        return new Response('<h1>Custom page</h1>');
     }
 }
